480189cb6e764db469c09e6f68a919cdfae269ea
SIIROM_0098 <SIIROM_0098@LTC-0098>
2016-07-26 16:43:06 +0300

    6

--- CRM2/src/Controller/hibernateUtil.java
+++ CRM2/src/Controller/hibernateUtil.java
@@ -1,21 +1,29 @@
 package Controller;
 
+import java.io.File;
+import java.io.FileReader;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
- 
 
 public class hibernateUtil {
+
+	static FileReader ceva = null;
+	static File ceva2 = null;
 
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 
 	private static SessionFactory buildSessionFactory() {
 		try {
+			ceva2 = new File(
+					"C:\\Users\\SIIROM_0098\\git\\java2\\CRM2\\resources\\hibernate.cfg.xml");
 
-			Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
-		//	configuration.addAnnotatedClass(Persoana.class);
+			Configuration configuration = new Configuration().configure(ceva2);
+			// configuration.addAnnotatedClass(Persoana.class);
 			return configuration
-					.buildSessionFactory(new StandardServiceRegistryBuilder().applySettings(configuration.getProperties())
+					.buildSessionFactory(new StandardServiceRegistryBuilder()
+							.applySettings(configuration.getProperties())
 							.build());
 
 		} catch (Exception e) {
